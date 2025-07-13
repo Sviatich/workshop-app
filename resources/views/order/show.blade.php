43ab68e7d8be7b3c2457514a58c6a32468069dac
Sviatoslav <48760990+Sviatich@users.noreply.github.com>
2025-07-13 15:29:23 +0300

    Improve label mappings in checkout and order views

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+    $colorMap = ['brown' => 'Бурый', 'white' => 'Белый'];
+    $strengthMap = ['econom' => 'Эконом', 'business' => 'Бизнес'];
+    $printTypeMap = [
+        'none' => 'Без оформления',
+        'print' => 'Печать',
+        'sticker' => 'Наклейка',
+        'wrapper' => 'Обечайка',
+    ];
+    $deliveryMap = [
+        'pickup' => 'Самовывоз',
+        'cdek' => 'СДЭК',
+        'pek' => 'ПЭК',
+        'own' => 'Свой курьер',
+    ];
+@endphp
     <div class="container">
         <h1>Спасибо за заказ!</h1>
 
@@ -9,7 +25,7 @@
         <p><strong>Email:</strong> {{ $order->customer_email }}</p>
         <p><strong>Телефон:</strong> {{ $order->customer_phone }}</p>
         <p><strong>Адрес доставки:</strong> {{ $order->delivery_address }}</p>
-        <p><strong>Тип доставки:</strong> {{ ucfirst($order->delivery_method) }}</p>
+        <p><strong>Тип доставки:</strong> {{ $deliveryMap[$order->delivery_method] ?? $order->delivery_method }}</p>
         <p><strong>Общая стоимость:</strong> {{ number_format($order->total_price, 2, ',', ' ') }} ₽</p>
 
         <h3>Позиции в заказе:</h3>
@@ -20,9 +36,9 @@
                     {{ $item->boxType->name ?? 'N/A' }},
                     {{ $item->length }}×{{ $item->width }}×{{ $item->height }} мм,
                     толщина: {{ $item->cardboard_thickness }} мм,
-                    цвет: {{ $item->cardboard_color }},
-                    прочность: {{ $item->cardboard_strength }},
-                    печать: {{ $item->print_type }},
+                    цвет: {{ $colorMap[$item->cardboard_color] ?? $item->cardboard_color }},
+                    прочность: {{ $strengthMap[$item->cardboard_strength] ?? $item->cardboard_strength }},
+                    печать: {{ $printTypeMap[$item->print_type] ?? $item->print_type }},
                     {{ number_format($item->price_per_box, 2, ',', ' ') }} ₽/шт
                     @if ($item->design_file)
                         , <a href="{{ Storage::disk('public')->url($item->design_file) }}" target="_blank">файл</a>
